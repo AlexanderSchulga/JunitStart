073ebd10f91b59486e0bf32c8f8b190689f1c093
Alexander <famakiller3@gmail.com>
2025-02-04 19:15:49 +0300

    Junit 3 файл все Параметризация, Exception, покрытие метода параметрами, сравнения equals Дополнил Desc.txt

--- src/test/java/tests/parametrize/TestNumberGreaterThat.java
+++ src/test/java/tests/parametrize/TestNumberGreaterThat.java
@@ -2,19 +2,16 @@ package tests.parametrize;
 
 import Pen.Pen;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import tests.BaseTest;
-import tests.PenGetColorArguments;
 
 
 public class TestNumberGreaterThat extends BaseTest {
     //Тест для второго метода в Pen
     @ParameterizedTest
     @ArgumentsSource(TestNumberGreaterThanArguments.class)//Другая аннотация
-    // и указываем источник PenGetColorArguments.class
+    // и указываем источник TestNumberGreaterThanArguments.class
     public void testPenWriteGetColorFoo(int numberToCheck, int numberToCompare, String expectedResult) {
 
         Pen pen = new Pen();//создаем обьект
